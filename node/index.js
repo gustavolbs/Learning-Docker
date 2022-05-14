b5c243a8401129b248a8c242c3ad1e8d023c35e8
Gustavo Bispo <38411510+gustavolbs@users.noreply.github.com>
2022-05-14 17:24:09 -0300

    add mysql and docker compose

--- node/index.js
+++ node/index.js
@@ -1,6 +1,20 @@
 const express = require("express");
+const mysql = require("mysql2");
 const app = express();
 const port = 3000;
+
+const config = {
+  host: "db",
+  user: "root",
+  password: "root",
+  database: "nodedb",
+};
+
+const connection = mysql.createConnection(config);
+
+const sql = `INSERT INTO people(name) values('Gustavo Bispo')`;
+connection.query(sql);
+connection.end();
 
 app.get("/", (req, res) => {
   res.send("<h1>My Node Image!</h1>");
